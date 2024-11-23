8e6ed7e9c5a249e630ea20f62e1dd7b0afd99aa7
kushaltrivedi5 <40542375+kushaltrivedi5@users.noreply.github.com>
2024-11-22 21:29:17 -0500

    Minor Structure Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/home";
-import Login from "./components/authentication/login";
-import Navbar from "./components/navbar"; // Import Navbar component
+import Home from "./components/Home";
+import Login from "./components/authentication/Login";
+import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound";
 
 const App = () => {
