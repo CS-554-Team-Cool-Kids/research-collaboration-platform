133e8ec191094d74f0c5656e421e1a63b739af68
kushaltrivedi5 <40542375+kushaltrivedi5@users.noreply.github.com>
2024-11-24 18:31:25 -0500

    basic font setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/common/Home";
 import Login from "./components/auth/Login";
-import Navbar from "./components/layout/Navbar";
-import NotFound from "./components/NotFound";
+import Navbar from "./components/common/Navbar";
+import NotFound from "./components/common/NotFound";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/authentication/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="container-fluid mt-3">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authentication/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -12,56 +12,54 @@ const Login = () => {
   };
 
   return (
-    <div className="container-fluid">
-      <div className="card col-12 col-md-4 glassEffect my-auto mx-auto">
-        <div className="card-header glassEffect">
-          <KeyIcon /> Login
-        </div>
-        <div className="card-body">
-          <form id="loginform" onSubmit={handleSubmit}>
-            <div className="form-floating mb-3">
-              <input
-                className="form-control"
-                type="email"
-                id="email"
-                name="email"
-                placeholder="name@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <label htmlFor="email">Email:</label>
-            </div>
-
-            <div className="form-floating mb-3">
-              <input
-                className="form-control"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <label htmlFor="password">Password:</label>
-            </div>
-
-            <div className="row">
-              <div className="offset-4 col-4 mt-3">
-                <button
-                  className="btn btn-warning col-12 py-2"
-                  type="submit"
-                  id="submit"
-                >
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-          <div className="mt-2">
-            <a href="/resetpassword">Forgot Password?</a>
+    <div className="card col-12 col-md-4 glassEffect my-auto mx-auto">
+      <div className="card-header glassEffect">
+        <KeyIcon /> Login
+      </div>
+      <div className="card-body">
+        <form id="loginform" onSubmit={handleSubmit}>
+          <div className="form-floating mb-3">
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="email">Email:</label>
           </div>
+
+          <div className="form-floating mb-3">
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+          </div>
+
+          <div className="row">
+            <div className="offset-4 col-4 mt-3">
+              <button
+                className="btn btn-warning col-12 py-2"
+                type="submit"
+                id="submit"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="mt-2">
+          <a href="/resetpassword">Forgot Password?</a>
         </div>
       </div>
     </div>
