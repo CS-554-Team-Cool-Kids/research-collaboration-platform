bfb95d2bd02d35c029be0be76cdd42a398b4fb9b
Connor Durkin <cdurkin@stevens.edu>
2024-12-08 13:51:12 -0500

    Hi all: adding into my branch the GraphQL files: resolvers, typeDefs and related helper functions. Also updated imports in app.js to match the file structure.
    Same code I previously shared via this temporary repository: https://github.com/csdurkin/sandbox/tree/main.
    
    Question to consider: I see the backend has a resolver folder, with resolvers broken down by student/professor. With the resolvers I'm adding, do we need these files? I think these are remnants.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { typeDefs } from "./typeDefs.js";
+import { typeDefs } from "./graphQl/typeDefs.js";
 // import { resolvers } from "./resolvers.js";
 import { resolvers } from "./resolvers/index.js";
 
--- backend/typeDefs.js
+++
@@ -1,6 +0,0 @@
-export const typeDefs = `#graphql
-    type Query{
-        welcome_professor: String,
-        welcome_student: String,
-    }
-`;
