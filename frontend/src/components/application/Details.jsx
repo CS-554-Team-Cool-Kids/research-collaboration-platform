0e7d900c23a6d8f3e332b5b8bf39ed7ffb26683e
Carmen G Vargas <carmengvargas@Carmens-MacBook-Pro.local>
2024-12-12 21:06:48 -0500

    Made updates to application page

--- frontend/src/components/application/Details.jsx
+++ frontend/src/components/application/Details.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useParams} from 'react-route-dom';
+import {useParams} from 'react-router-dom';
 import {useQuery} from '@apollo/client';
 import queries from '../../queries';
 
