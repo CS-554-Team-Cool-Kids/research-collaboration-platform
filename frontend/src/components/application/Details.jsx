27ebfc608b37a9b42bb2fff6038046538106edc1
Carmen G Vargas <carmengvargas@Carmens-MacBook-Pro.local>
2024-12-12 19:33:24 -0500

    Updated applications list and detail pages

--- frontend/src/components/application/Details.jsx
+++ frontend/src/components/application/Details.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useParams} from 'react-route-dom';
 import {useQuery} from '@apollo/client';
 import queries from '../../queries';
 
@@ -11,9 +12,57 @@ function ApplicationDetails(props) {
             d. Application status
             e. Application comments
     */
-   return (
-    <p>Application details page pending...</p>
-   );
+   
+    const {id} = useParams();
+    const applicationData = useQuery(queries.GET_APPLICATION_BY_ID, {
+        variables: { id },
+        fetchPolicy: 'cache-and-network'
+    });
+    const application = applicationData.data.getApplicationById;
+    const loading = applicationData.loading;
+    const error = applicationData.error;
+    
+    if(loading){
+        return (
+            <p>Loading..</p>
+        );
+    } else if (error){
+        return (
+            <p>Error: {error}</p>
+        );
+    } else {
+        return (
+            <main className="dashboard">
+            <ActionBar />
+            <div className="container-fluid my-3">
+                <div className="d-card glassEffect">
+                    <div className="d-card-header">Application Details</div>
+                    <div className="d-card-body">
+                        <dl className="desc-list">
+                            <div>
+                                <dt>Project Title</dt>
+                                <dd>{application.project.title}</dd>
+                            </div>
+                            <div>
+                                <dt>Lead Professor</dt>
+                                <dd>{application.project.professor.firstName} {application.project.professor.lastName}</dd>
+                            </div>
+                            <div>
+                                <dt>Application Date</dt>
+                                <dd>{application.applicationDate}</dd>
+                            </div>
+                            <div>
+                                <dt>Application Status</dt>
+                                <dd>{application.applicationStatus}</dd>
+                            </div>
+                        </dl>
+                        {/* Add comments */}
+                    </div>
+                </div>
+            </div>
+            </main>
+        );
+    }
 
 }
 
--- frontend/src/components/application/List.jsx
+++ frontend/src/components/application/List.jsx
@@ -17,15 +17,21 @@ function ApplicationList(props) {
             c. button to view the details, which would redirect to the application/detail page.
             Note: the queries file was updated to ensure the necessary pieces of data are passed.
     */
-    {/* 
-    const id = props.userId;
+   
+    /* Client Side Logic:
 
+    const id = props.userId;
     const applicationData = useQuery(queries.GET_USER_BY_ID, {
         variables: { id },
         fetchPolicy: 'cache-and-network'
     });
     const applicationsList = applicationData.data.getUserById.applications;
-    */}
+
+    const loading = applicationData.loading;
+    const error = applicationData.error;
+    */
+
+    /* Temporary (Dummy) Data */
     const applicationsList = [
         {
             _id: "001",
@@ -94,6 +100,9 @@ function ApplicationList(props) {
             numOfComments: 2
         }
     ]
+    const loading = false;
+    const error = false;
+    /* End of Temporary Data */
     const [applications, setApplications] = useState([]);
     const [selectedApplication, setSelectedApplication] = useState(null);
 
@@ -102,65 +111,75 @@ function ApplicationList(props) {
         setApplications(props.applications || applicationsList || []);
     }, [props.applications]);
 
-    return (
-        <main className="dashboard">
-            <div className="container my-3">
-                <div className="d-card glassEffect">
-                    <div className="d-card-header">
-                        <div className="row col-12 d-flex">
-                            <div className="col my-auto">
-                                <h2>Application List</h2>
-                            </div>
-                            <div className="col-auto">
-                                {selectedApplication?._id ? (
-                                    <Link
-                                        className="nav-link"
-                                        to={"/application/details/" + selectedApplication?._id}
-                                    >
-                                        <div className="btn btn-info">Details</div>
-                                    </Link>
-                                ) : (
-                                    <div className="btn btn-info invisible">Invisible Button</div>
-                                )}
+    if(loading){
+        return (
+            <p>Loading...</p>
+        );
+    } else if (error){
+        return (
+            <p>Error: {error}</p>
+        );
+    } else {
+        return (
+            <main className="dashboard">
+                <div className="container my-3">
+                    <div className="d-card glassEffect">
+                        <div className="d-card-header">
+                            <div className="row col-12 d-flex">
+                                <div className="col my-auto">
+                                    <h2>Application List</h2>
+                                </div>
+                                <div className="col-auto">
+                                    {selectedApplication?._id ? (
+                                        <Link
+                                            className="nav-link"
+                                            to={"/application/details/" + selectedApplication?._id}
+                                        >
+                                            <div className="btn btn-info">Details</div>
+                                        </Link>
+                                    ) : (
+                                        <div className="btn btn-info invisible">Invisible Button</div>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="d-card-body p-0">
-                        <div className="row">
-                            {/* Left Side: Application List */}
-                            <div className="col-md-4">
-                                <ul className="chat-list">
-                                    {applications.map((application, index) => (
-                                        <li 
-                                            key={application._id}
-                                            onClick={() => setSelectedApplication(application)}
-                                            className={
-                                                selectedApplication?._id === application._id ? "active" : ""
-                                            }
-                                        >
-                                            <span className="chat-list-number">{index + 1}.</span>
-                                            <p className="chat-list-header">application.project.title</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            {/* Right Side: Reading Pane */}
-                            <div className="col-md-8 my-3 border-start">
-                                {selectedApplication ? (
-                                    <div>
-                                        <h2>{selectedApplication.project.title}</h2>
-                                        <p>{selectedApplication.status}</p>
-                                    </div>
-                                ) : (
-                                    <p>Select an application to view its status</p>
-                                )}
+                        <div className="d-card-body p-0">
+                            <div className="row">
+                                {/* Left Side: Application List */}
+                                <div className="col-md-4">
+                                    <ul className="chat-list">
+                                        {applications.map((application, index) => (
+                                            <li 
+                                                key={application._id}
+                                                onClick={() => setSelectedApplication(application)}
+                                                className={
+                                                    selectedApplication?._id === application._id ? "active" : ""
+                                                }
+                                            >
+                                                <span className="chat-list-number">{index + 1}.</span>
+                                                <p className="chat-list-header">application.project.title</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                {/* Right Side: Reading Pane */}
+                                <div className="col-md-8 my-3 border-start">
+                                    {selectedApplication ? (
+                                        <div>
+                                            <h2>{selectedApplication.project.title}</h2>
+                                            <p>{selectedApplication.status}</p>
+                                        </div>
+                                    ) : (
+                                        <p>Select an application to view its status</p>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
-    );
+            </main>
+        );
+    }
 }
 
 export default ApplicationList;
