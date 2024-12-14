5a1c6868d70c4762e6407105f4104e016109f450
ahujagautam024 <ahujagautam024@gmail.com>
2024-12-14 12:25:01 -0500

    login fixed

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext"; // Import your AuthContext
 import DarkMode from "../../assets/svg/DarkMode";
 import LightMode from "../../assets/svg/LightMode";
 
 const Navbar = () => {
+  const { authState, logout } = useAuth(); // Use the AuthContext to get authState and logout function
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -23,26 +26,43 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
+            {/* Always show Home */}
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/register">
-                Register
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/project">
-                Project
-              </Link>
-            </li>
+
+            {/* Show links based on authentication */}
+            {!authState.isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/auth/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/auth/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/project">
+                    Project
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link btn btn-link" onClick={logout}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            )}
+
+            {/* Theme toggle */}
             <li className="ms-5">
               <div className="d-flex">
                 <div className="my-auto ms-auto">
