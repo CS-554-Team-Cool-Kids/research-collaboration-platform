005b23d0fcfef0cb72082fde43660f6ab70c67c2
ahujagautam024 <ahujagautam024@gmail.com>
2024-12-14 12:06:22 -0500

    some changes to messaging tool

---
+++ backend/messageOperations/index.js
@@ -0,0 +1,17 @@
+import { messages as messageCollection } from "../config/mongoCollections.js";
+
+export async function addMessage(message) {
+  const messages = await messageCollection();
+  await messages.insertOne(message);
+}
+
+export async function getMessagesByChannel(channelId) {
+  const messages = await messageCollection();
+  const allMessages = await messages
+    .find({ channelId })
+    .sort({ timestamp: -1 })
+    .limit(50)
+    .toArray();
+
+  return allMessages;
+}
